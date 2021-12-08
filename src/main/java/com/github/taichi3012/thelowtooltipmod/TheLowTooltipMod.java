b940dadc643004eeedc4e4a245e05441222d08bd
taichi3012 <ayako.taichi.haru@gmail.com>
2021-12-09 06:34:31 +0900

    v1.1.1

--- src/main/java/com/github/taichi3012/thelowtooltipmod/TheLowTooltipMod.java
+++ src/main/java/com/github/taichi3012/thelowtooltipmod/TheLowTooltipMod.java
@@ -33,7 +33,7 @@ import static com.github.taichi3012.thelowtooltipmod.api.TheLowAPI.requestPlayer
 @Mod(modid = TheLowTooltipMod.MOD_ID, version = TheLowTooltipMod.VERSION, name = TheLowTooltipMod.MOD_NAME, guiFactory = "com.github.taichi3012.thelowtooltipmod.gui.TheLowTooltipModGuiFactory")
 public class TheLowTooltipMod {
     public static final String MOD_ID = "thelowtooltipmod";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     public static final String MOD_NAME = "TheLowTooltipMod";
 
     public static Logger logger;
