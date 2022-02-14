1ec98ba65d7b7d954a7e08a164cf22ab4378a2a1
taichi3012 <ayako.taichi.haru@gmail.com>
2022-02-15 05:37:10 +0900

    Tooltipを変更するイベントを移動

--- src/main/java/com/github/taichi3012/thelowtooltipmod/TheLowTooltipMod.java
+++ src/main/java/com/github/taichi3012/thelowtooltipmod/TheLowTooltipMod.java
@@ -3,17 +3,13 @@ package com.github.taichi3012.thelowtooltipmod;
 import com.github.taichi3012.thelowtooltipmod.api.TheLowAPI;
 import com.github.taichi3012.thelowtooltipmod.config.TheLowTooltipModConfig;
 import com.github.taichi3012.thelowtooltipmod.listener.GuiParkSelectorListener;
-import com.github.taichi3012.thelowtooltipmod.util.TheLowNBTUtil;
-import com.github.taichi3012.thelowtooltipmod.util.TheLowUtil;
-import com.github.taichi3012.thelowtooltipmod.weapon.*;
-import com.github.taichi3012.thelowtooltipmod.weapon.skill.IWeaponSkillAble;
+import com.github.taichi3012.thelowtooltipmod.listener.TooltipListener;
 import com.github.taichi3012.thelowtooltipmod.weapon.skill.SkillManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -45,6 +41,7 @@ public class TheLowTooltipMod {
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new GuiParkSelectorListener());
+        MinecraftForge.EVENT_BUS.register(new TooltipListener());
 
         SkillManager.registerAll();
     }
@@ -69,117 +66,6 @@ public class TheLowTooltipMod {
     public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
         if (event.modID.equals(TheLowTooltipMod.MOD_ID)) {
             TheLowTooltipModConfig.syncConfig();
-        }
-    }
-
-    @SubscribeEvent
-    public void onItemTooltip(ItemTooltipEvent event) {
-        if (TheLowTooltipModConfig.isTooltipEnable() && GuiScreen.isCtrlKeyDown()) {
-            addResult(event);
-        }
-    }
-
-    private void addResult(ItemTooltipEvent event) {
-        ItemStack stack = event.itemStack;
-
-        if (stack.getTagCompound() == null) {
-            return;
-        }
-
-        if (TheLowNBTUtil.hasDamage(event.itemStack)) {
-            WeaponBasic weapon = null;
-            requestPlayerStatus(false);
-
-            String skillSetId = TheLowNBTUtil.getSkillSetID(stack);
-            if (TheLowUtil.isMagicMixtureWeapon(event.itemStack)) {
-                weapon = new WeaponMagicalMixture(stack);
-            } else if (skillSetId != null) {
-                switch (skillSetId) {
-                    case "12":
-                        weapon = new WeaponPlaceLight(stack);
-                        break;
-                    case "36":
-                        weapon = new WeaponGekokujo(stack);
-                        break;
-                }
-            }
-
-            if (weapon == null) {
-                weapon = new WeaponBasic(stack);
-            }
-
-            //アイテムの名前と結果のみ表示する設定が有効なときはここで返す。
-            if (TheLowTooltipModConfig.isOnlyDisplayResultAndNameEnable()) {
-                String itemDisplayName = event.toolTip.get(0);
-                event.toolTip.clear();
-                event.toolTip.add(itemDisplayName);
-                event.toolTip.addAll(weapon.generateResultContext());
-                return;
-            }
-
-            int cursor = event.showAdvancedItemTooltips ? event.toolTip.size() - 2 : event.toolTip.size();
-            for (String str : event.toolTip) {
-                if (str.contains("[SLOT]")) {
-                    cursor = event.toolTip.indexOf(str);
-                    break;
-                }
-            }
-
-            event.toolTip.addAll(cursor, weapon.generateResultContext());
-            cursor += weapon.generateResultContext().size();
-            event.toolTip.add(cursor, "");
-
-            return;
-        }
-
-        if (stack.getTagCompound().hasKey("view_weapon_skill_id")) {
-            IWeaponSkillAble skill = SkillManager.getSkill(stack.getTagCompound().getString("view_weapon_skill_id"));
-            ItemStack heldStack = Minecraft.getMinecraft().thePlayer.getCurrentEquippedItem();
-            requestPlayerStatus(false);
-
-            if (skill != null && heldStack != null && TheLowNBTUtil.hasDamage(heldStack) && skill.isActive(new WeaponData(heldStack))) {
-                List<String> result = new ArrayList<>();
-                WeaponData weaponData = new WeaponData(heldStack);
-
-                List<String> resultContext = skill.getResultContext(weaponData);
-                if (TheLowTooltipModConfig.isSkillResultContextEnable() && !resultContext.isEmpty()) {
-                    result.addAll(skill.getResultContext(weaponData));
-                }
-
-                List<String> coolTimeContext = skill.getCoolTimeContext(weaponData);
-                if (TheLowTooltipModConfig.isSkillCoolTimeContextEnable() && !coolTimeContext.isEmpty()) {
-                    if (!result.isEmpty()) {
-                        result.add("");
-                    }
-                    result.addAll(coolTimeContext);
-                }
-
-                //結果がないときは返す。
-                if (result.isEmpty()) {
-                    return;
-                }
-
-                //アイテムの名前と結果のみ表示する設定が有効なときはここで返す。
-                if (TheLowTooltipModConfig.isOnlyDisplayResultAndNameEnable()) {
-                    String itemDisplayName = event.toolTip.get(0);
-                    event.toolTip.clear();
-                    event.toolTip.add(itemDisplayName);
-                    event.toolTip.addAll(result);
-                    return;
-                }
-
-                int cursor = event.showAdvancedItemTooltips ? event.toolTip.size() - 2 : event.toolTip.size();
-                for (String str : event.toolTip) {
-                    if (str.contains("スキル発動中") || str.contains("クリックしてスキルを")) {
-                        cursor = event.toolTip.indexOf(str);
-                        break;
-                    }
-                }
-
-                event.toolTip.addAll(cursor, result);
-                cursor += result.size();
-                event.toolTip.add(cursor, "");
-            }
         }
     }
 }
---
+++ src/main/java/com/github/taichi3012/thelowtooltipmod/listener/TooltipListener.java
@@ -0,0 +1,134 @@
+package com.github.taichi3012.thelowtooltipmod.listener;
+
+import com.github.taichi3012.thelowtooltipmod.config.TheLowTooltipModConfig;
+import com.github.taichi3012.thelowtooltipmod.util.TheLowNBTUtil;
+import com.github.taichi3012.thelowtooltipmod.util.TheLowUtil;
+import com.github.taichi3012.thelowtooltipmod.weapon.*;
+import com.github.taichi3012.thelowtooltipmod.weapon.skill.IWeaponSkillAble;
+import com.github.taichi3012.thelowtooltipmod.weapon.skill.SkillManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.github.taichi3012.thelowtooltipmod.api.TheLowAPI.requestPlayerStatus;
+
+/**
+ * @author taichi1230
+ */
+public class TooltipListener {
+    @SubscribeEvent
+    public void onItemTooltip(ItemTooltipEvent event) {
+        if (TheLowTooltipModConfig.isTooltipEnable() && GuiScreen.isCtrlKeyDown()) {
+            addResult(event);
+        }
+    }
+
+    private void addResult(ItemTooltipEvent event) {
+        ItemStack stack = event.itemStack;
+
+        if (stack.getTagCompound() == null) {
+            return;
+        }
+
+        if (TheLowNBTUtil.hasDamage(event.itemStack)) {
+            WeaponBasic weapon = null;
+            requestPlayerStatus(false);
+
+            String skillSetId = TheLowNBTUtil.getSkillSetID(stack);
+            if (TheLowUtil.isMagicMixtureWeapon(event.itemStack)) {
+                weapon = new WeaponMagicalMixture(stack);
+            } else if (skillSetId != null) {
+                switch (skillSetId) {
+                    case "12":
+                        weapon = new WeaponPlaceLight(stack);
+                        break;
+                    case "36":
+                        weapon = new WeaponGekokujo(stack);
+                        break;
+                }
+            }
+
+            if (weapon == null) {
+                weapon = new WeaponBasic(stack);
+            }
+
+            //アイテムの名前と結果のみ表示する設定が有効なときはここで返す。
+            if (TheLowTooltipModConfig.isOnlyDisplayResultAndNameEnable()) {
+                String itemDisplayName = event.toolTip.get(0);
+                event.toolTip.clear();
+                event.toolTip.add(itemDisplayName);
+                event.toolTip.addAll(weapon.generateResultContext());
+                return;
+            }
+
+            int cursor = event.showAdvancedItemTooltips ? event.toolTip.size() - 2 : event.toolTip.size();
+            for (String str : event.toolTip) {
+                if (str.contains("[SLOT]")) {
+                    cursor = event.toolTip.indexOf(str);
+                    break;
+                }
+            }
+
+            event.toolTip.addAll(cursor, weapon.generateResultContext());
+            cursor += weapon.generateResultContext().size();
+            event.toolTip.add(cursor, "");
+
+            return;
+        }
+
+        if (stack.getTagCompound().hasKey("view_weapon_skill_id")) {
+            IWeaponSkillAble skill = SkillManager.getSkill(stack.getTagCompound().getString("view_weapon_skill_id"));
+            ItemStack heldStack = Minecraft.getMinecraft().thePlayer.getCurrentEquippedItem();
+            requestPlayerStatus(false);
+
+            if (skill != null && heldStack != null && TheLowNBTUtil.hasDamage(heldStack) && skill.isActive(new WeaponData(heldStack))) {
+                List<String> result = new ArrayList<>();
+                WeaponData weaponData = new WeaponData(heldStack);
+
+                List<String> resultContext = skill.getResultContext(weaponData);
+                if (TheLowTooltipModConfig.isSkillResultContextEnable() && !resultContext.isEmpty()) {
+                    result.addAll(skill.getResultContext(weaponData));
+                }
+
+                List<String> coolTimeContext = skill.getCoolTimeContext(weaponData);
+                if (TheLowTooltipModConfig.isSkillCoolTimeContextEnable() && !coolTimeContext.isEmpty()) {
+                    if (!result.isEmpty()) {
+                        result.add("");
+                    }
+                    result.addAll(coolTimeContext);
+                }
+
+                //結果がないときは返す。
+                if (result.isEmpty()) {
+                    return;
+                }
+
+                //アイテムの名前と結果のみ表示する設定が有効なときはここで返す。
+                if (TheLowTooltipModConfig.isOnlyDisplayResultAndNameEnable()) {
+                    String itemDisplayName = event.toolTip.get(0);
+                    event.toolTip.clear();
+                    event.toolTip.add(itemDisplayName);
+                    event.toolTip.addAll(result);
+                    return;
+                }
+
+                int cursor = event.showAdvancedItemTooltips ? event.toolTip.size() - 2 : event.toolTip.size();
+                for (String str : event.toolTip) {
+                    if (str.contains("スキル発動中") || str.contains("クリックしてスキルを")) {
+                        cursor = event.toolTip.indexOf(str);
+                        break;
+                    }
+                }
+
+                event.toolTip.addAll(cursor, result);
+                cursor += result.size();
+                event.toolTip.add(cursor, "");
+            }
+        }
+    }
+}
